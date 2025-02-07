3fadec9d1b8f997f23bf69dbfd385cabf15d6206
Chung <nguyentranchung52th@gmail.com>
2025-02-07 20:49:06 +0900

    fix full blank password

--- app/Filament/Resources/UserResource/Pages/EditUser.php
+++ app/Filament/Resources/UserResource/Pages/EditUser.php
@@ -16,4 +16,13 @@ class EditUser extends EditRecord
             Actions\DeleteAction::make(),
         ];
     }
+
+    protected function mutateFormDataBeforeSave(array $data): array
+    {
+        if (! $data['password']) {
+            unset($data['password']);
+        }
+
+        return $data;
+    }
 }
